5885593eb1d3a7ce35c4098f617e986448b30a21
mones <mones.cse@gmail.com>
2023-02-02 20:13:36 +0600

    added prettier

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,31 @@
-import './globals.css'
+import './globals.css';
+import Link from 'next/link';
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang='en'>
       {/*
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
       <head />
-      <body>{children}</body>
+      <body>
+        <nav>
+          <ul>
+            <li>
+              <Link href='/'>Home</Link>
+            </li>
+            <li>
+              <Link href='/about'>About Us</Link>
+            </li>
+          </ul>
+        </nav>
+        {children}
+      </body>
     </html>
-  )
+  );
 }
